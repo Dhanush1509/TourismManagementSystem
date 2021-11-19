cbaaa26c0a87a756d6b8693a44a0d57994160903
Dhanush1509 <s.munidhanush15@gmail.com>
2021-11-20 00:36:45 +0530

    add changes

--- backend/middlewares/setAuthToken.js
+++ backend/middlewares/setAuthToken.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User.js");
+const User = require("../backend/models/User.js.js");
 const dotenv = require("dotenv");
 dotenv.config();
 
